416ae5fad2a8aa18836e46604ac818bbd1c49245
Azraf Al Monzim <dev@monzim.com>
2024-05-09 10:40:56 +0600

    Refactor import statements for unizim_models_base.dart

--- lib/src/unizim_models_base.dart
+++ lib/src/unizim_models_base.dart
@@ -1,8 +1,8 @@
-export './models/event_model.dart';
-export './models/fcm_payload_model.dart';
-export '././constants/databases.dart';
-export '././constants/collections.dart';
-export './extentions/server_time.dart';
-export './models/topic_channel.dart';
-export './models/image_compression.dart';
-export '././constants/storages.dart';
+export 'constants/collections.dart';
+export 'constants/databases.dart';
+export 'constants/storages.dart';
+export 'extentions/server_time.dart';
+export 'models/event_model.dart';
+export 'models/fcm_payload_model.dart';
+export 'models/image_compression.dart';
+export 'models/topic_channel.dart';

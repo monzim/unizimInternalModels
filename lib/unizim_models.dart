cf008646bfb5fdfa8aa60233117105ef89effde1
Azraf Al Monzim <dev@monzim.com>
2024-05-09 10:50:45 +0600

    chore: Update unizim_models.dart exports and remove unused code

--- lib/unizim_models.dart
+++ lib/unizim_models.dart
@@ -1,3 +1,10 @@
 library unizim_models;
 
-export 'src/unizim_models_base.dart';
+export 'src/constants/collections.dart';
+export 'src/constants/databases.dart';
+export 'src/constants/storages.dart';
+export 'src/extentions/server_time.dart';
+export 'src/models/event_model.dart';
+export 'src/models/fcm_payload_model.dart';
+export 'src/models/image_compression.dart';
+export 'src/models/topic_channel.dart';
